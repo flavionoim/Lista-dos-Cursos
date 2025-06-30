1df7928e55b71879df72c08e8819b55f2b729f5b
Flavio Gustavo Noim de Santana <150524667+flavionoim@users.noreply.github.com>
2025-06-30 17:25:02 -0300

    Add files via upload

--- ambiente.js
+++ ambiente.js
@@ -1,21 +1,26 @@
 function cursos() {
     let lista = document.getElementById("lista");
-    let cursos = ["Inglês", "Espanhol", "Alemão", "Mandarim"];
-  
+    let cursos = [
+        { id: 1, curso: "Inglês" },
+        { id: 2, curso: "Espanhol" },
+        { id: 3, curso: "Alemão" },
+        { id: 4, curso: "Mandarim" },
+    ];
+
     lista.innerHTML = "Os cursos que temos são:";
-  
+
     let ul = document.createElement("ul");
-  
-    cursos.forEach(function (curso) {
-      let li = document.createElement("li");
-      li.textContent = curso;
-      ul.appendChild(li);
+
+    cursos.forEach(function (CURSO) {
+        let li = document.createElement("li");
+        li.textContent = CURSO.curso; 
+        ul.appendChild(li);
     });
-  
+
     lista.appendChild(ul);
-  }
+}
 
-
-  function ocultcursos() {
-    lista.innerHTML = ''
-  }
+function ocultcursos() {
+    let lista = document.getElementById("lista");
+    lista.innerHTML = ""; 
+}
